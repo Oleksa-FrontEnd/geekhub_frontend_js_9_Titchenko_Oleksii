00ede568ed32b13de4182dd9289ff1b279440e2c
-=EL=- <IT0671581471@gmail.com>
2019-11-06 00:14:12 +0200

    section social slider Doctor (not button)

--- Lesson 3 (forkio)/app/js/main.js
+++ Lesson 3 (forkio)/app/js/main.js
@@ -1,7 +1,17 @@
 $('.menu-btn').on('click', function(e) {
     e.preventDefault;
     $(this).toggleClass('menu-btn_active');
-    //$('.menu-nav').toggleClass('menu-nav_active');
+    $('.menu-nav').toggleClass('menu-nav_active');
     $('.nav-wrap').toggleClass('.nav-wrap_active');
     $('.nav-element').toggleClass('.nav-element_active');
 });
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,3000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
