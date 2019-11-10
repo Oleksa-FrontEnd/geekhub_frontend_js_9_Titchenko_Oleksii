b209d7677b4d644f235e0f65d01116db92a4d6e4
-=EL=- <IT0671581471@gmail.com>
2019-11-10 18:25:25 +0200

    adaptiv section snippets

---
+++ Lesson 2 (Eskulap Template)/app/js/main.js
@@ -0,0 +1,18 @@
+$('.menu-btn').on('click', function(e) {
+    e.preventDefault;
+    $(this).toggleClass('menu-btn_active');
+    //$('.menu-nav').toggleClass('menu-nav_active');
+    $('.nav-wrap').toggleClass('.nav-wrap_active');
+    $('.nav-element').toggleClass('.nav-element_active');
+});
+
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
